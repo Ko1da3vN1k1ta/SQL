cbfbca3863806ae1e7790f8df397f10bda2dca9d
Ko1da3vN1k1ta <maniac779@mail.ru>
2023-10-16 14:17:35 +0300

    ^*

--- src/main/java/org/example/JavaSchoolStarter.java
+++ src/main/java/org/example/JavaSchoolStarter.java
@@ -1,46 +1,55 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class JavaSchoolStarter {
-    List<Map<String, Object>> data;
+    Table table = new Table();
+    Scanner cr = new Scanner(System.in);
 
-    public List<Map<String, Object>> execute(String request) {
-        data = new ArrayList<>();
-        requestOrder(request);
-        return null;
+    public JavaSchoolStarter() {
+        Table table = new Table();
     }
 
-    private void requestOrder(String request) {
+    public List<Map<String, Object>> execute(String request) throws Exception {
+
+        readOrder(request);
+
+
+        return table.getData();
+    }
+
+    private void readOrder(String request) {
         if (request.startsWith("INSERT")) {
-            insertValues();
-        }
-       else if (request.startsWith("UPDATE")) {
+            insertValues(request);
+        } else if (request.startsWith("UPDATE")) {
             updateValues();
-        }
-        else if (request.startsWith("DELETE")) {
+        } else if (request.startsWith("DELETE")) {
             deleteValues();
-        }
-       else if (request.startsWith("SELECT")){
+        } else if (request.startsWith("SELECT")) {
             selectValues();
         }
     }
 
-    private void selectValues() {
+    private void deleteValues() {
+
     }
 
-    private void deleteValues() {
-        
+    private void selectValues() {
+
     }
 
     private void updateValues() {
-        
+
     }
 
-    private void insertValues() {
-        
+    private void insertValues(String request) {
+        Map<String, Object> row3 = new HashMap<>();
+        Pattern pattern = Pattern.compile("\"([^\"]*)\"");
+        Matcher matcher = pattern.matcher(request);
+
+        table.getData().add(row3);
+
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,10 +7,11 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         JavaSchoolStarter javaSchoolStarter = new JavaSchoolStarter();
-        javaSchoolStarter.createTable();
-        javaSchoolStarter.printTable();
+        List<Map<String,Object>> result1 = javaSchoolStarter.execute("INSERT VALUES " +
+                "'lastName' = 'Федоров' , " +
+                "'id'=3, 'age'=40, 'active'=true");
 
     }
 }
